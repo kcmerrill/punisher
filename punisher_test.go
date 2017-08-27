3800500f6272cf938a85d08032db51cb3c5ffa55
kc merrill <kcmerrill@gmail.com>
2017-08-26 23:51:56 -0600

    Update punisher_test.go

--- punisher_test.go
+++ punisher_test.go
@@ -1,7 +1,16 @@
 package main
 
-import "testing"
+import (
+	"path/filepath"
+	"testing"
+	"time"
+)
 
-func TestSomething(t *testing.T) {
-
+func TestSimplePunisher(t *testing.T) {
+	punish(time.Second, 200*time.Millisecond, 1, "touch /tmp/punisher.$(date | md5)")
+	//only one should have been created
+	files, globErr := filepath.Glob("/tmp/punisher*")
+	if len(files) >= 2 || globErr != nil {
+		t.Fatalf("Expected only 1 file to be created.")
+	}
 }
